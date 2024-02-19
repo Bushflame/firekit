75f238e6266a7c3ac9ca6dda78685f4b811c0a7f
David Glover <davidglover@boxedfx.com>
2024-02-19 17:02:03 +0000

    test vercel

--- src/lib/firebase/firebase.server.js
+++ src/lib/firebase/firebase.server.js
@@ -1,16 +1,23 @@
 import admin from 'firebase-admin';
-import serviceAccount from './firebase-secrets.server.json';
-
-import {GOOGLE_SERVICE_ACCOUNT} from '$env/static/private'
-if (admin.apps.length === 0) {
+import * as credential from './firebase-secrets.server.json';
+//import creds {GOOGLE_SERVICE_ACCOUNT} from '$env/static/private'
+	if (admin.apps.length === 0) {
+		console.log('bollox',credential)
 	admin.initializeApp({
-		// @ts-ignore
-		//credential: admin.credential.cert(JSON.parse(GOOGLE_SERVICE_ACCOUNT))
-		Credential: admin.credential.cert(serviceAccount)
+
+		//Credential: admin.credential.cert(JSON.parse(GOOGLE_SERVICE_ACCOUNT))
+		//PUBLIC_PROJECT_ID=sveltefire-e88f7
+		projectId: 'sveltefire-e88f7',
+		 credential: admin.credential.cert(credential)
+		//credential: admin.credential.cert(GOOGLE_SERVICE_ACCOUNT)
+		//credential: admin.credential.cert(JSON.parse(serviceAccount ))
+		
 	});
+
 } else {
-	console.log('bollox',)
+	
 }
+
 
 export const db = admin.firestore();
 export const auth = admin.auth();
